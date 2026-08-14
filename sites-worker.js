2e62805b6a6a4ccc4195121fdb0285a52fda1d15
jaretpetras <jaret.petras@neuro-football.com>
2026-08-14 01:27:57 -0400

    Fix Sites static asset routing

--- sites-worker.js
+++ sites-worker.js
@@ -1,10 +1,18 @@
 export default {
   async fetch(request, env) {
-    const response = await env.ASSETS.fetch(request)
-    if (response.status !== 404) return response
+    const directResponse = await env.ASSETS.fetch(request)
+    if (directResponse.status !== 404) return directResponse
+
+    const url = new URL(request.url)
+    if (url.pathname.startsWith("/assets/")) {
+      const assetUrl = new URL(request.url)
+      assetUrl.pathname = `/dist${url.pathname}`
+      const assetResponse = await env.ASSETS.fetch(new Request(assetUrl, request))
+      if (assetResponse.status !== 404) return assetResponse
+    }
 
     const indexUrl = new URL(request.url)
-    indexUrl.pathname = "/index.html"
+    indexUrl.pathname = "/dist/index.html"
     indexUrl.search = ""
     return env.ASSETS.fetch(new Request(indexUrl, request))
   },
